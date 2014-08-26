3c5fb7762c97048d848d312e615b27e4e82b207d
lester <tkhdlstfl.l+git@gmail.com>
2014-08-27 02:00:42 +0800

    Add test for generating lines from string

--- tests.py
+++ tests.py
@@ -5,3 +5,7 @@ from pylm import m
 
 def test_when_piped_returns_generator():
     assert_that('' | m, instance_of(collections.Iterable))
+
+def test_generator_yields_lines_from_the_text():
+    a = 'abc\nbd\n\nccc' | m
+    assert_that(a, contains('abc', 'bd', '', 'ccc'))
