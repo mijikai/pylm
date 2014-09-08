09e9505218f4cea142d82bb70b5dae7e9218e5b7
lester <tkhdlstfl.l+git@gmail.com>
2014-09-08 23:30:43 +0800

    Add filter by indexing a character

--- pylm.py
+++ pylm.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
+import operator
 
 
 class PylmOneLine:
+    def __init__(self, func=None):
+        self._func = func
+
     def __ror__(self, other):
-        yield from other.split('\n')
+        res = other.split('\n')
+        if self._func is None:
+            yield from res
+        else:
+            yield from map(self._func, res)
+
+    def __getitem__(self, index):
+        return PylmOneLine(lambda line: line[index])
 
 m = PylmOneLine()
--- tests.py
+++ tests.py
@@ -9,3 +9,8 @@ def test_when_piped_returns_generator():
 def test_generator_yields_lines_from_the_text():
     a = 'abc\nbd\n\nccc' | m
     assert_that(a, contains('abc', 'bd', '', 'ccc'))
+
+def test_retrieve_a_character_with_index_for_all_lines():
+    text = 'ajkw\nbfkad\noak#2'
+    assert_that(text | m[0], contains('a', 'b', 'o'))
+    assert_that(text | m[3], contains('w', 'a', '#'))
