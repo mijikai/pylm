790910cec86e01bf7c93925209310132a24557eb
lester <tkhdlstfl.l+git@gmail.com>
2014-09-23 02:02:53 +0800

    Fix skipping of not yet functional test
    The documentation was misread. When pytest.skip is run, it will stop or ignore
    whatever the context it is running so when one runs py.test, nothing is
    collected. Add a wrapper and use that for skipping the test.

--- tests.py
+++ tests.py
@@ -1,8 +1,18 @@
 from hamcrest import *
 import collections
+import functools
 import pytest
 
 from pylm import m
+
+
+def skip_test(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        pytest.skip()
+        func(*args, **kwargs)
+    return wrapper
+
 
 def test_when_piped_returns_generator():
     assert_that('' | m, instance_of(collections.Iterable))
@@ -16,22 +26,22 @@ def test_retrieve_a_character_with_index_for_all_lines():
     assert_that(text | m[0], contains('a', 'b', 'o'))
     assert_that(text | m[3], contains('w', 'a', '#'))
 
-@pytest.skip
+@skip_test
 def test_append_characters_to_each_other():
     assert_that('abc\nxyz' | m[0] + m[-1], contains('ac', 'xz'))
     assert_that('abc\ndef\nghi' | m[-1] + m[1], contains('cb', 'fe', 'ih'))
 
-@pytest.skip
+@skip_test
 def test_select_range_of_characters_from_line():
     assert_that('thequickbrownfox\njumpsoverthelazy' | m[2:7],
             contains('equic', 'mpsov'))
 
-@pytest.skip
+@skip_test
 def test_select_range_of_characters_from_line_with_step():
     assert_that('thequickbrownfox\njumpsoverthelazy' | m[2:7:2],
             contains('euc', 'msv'))
 
-@pytest.skip
+@skip_test
 def test_append_a_string_to_each_line():
     assert_that('hello\nworld\nprogramming' | m + ' you sss',
             contains('hello you sss', 'world you sss', 'programming you sss'))
